6aa544eebc1b2257772a8ecd43a7fb540a15107e
swati2904 <swati.lpu17@gmail.com>
2020-05-29 04:44:41 +0530

    level  1 implementation added

--- js/index.js
+++ js/index.js
@@ -11,10 +11,12 @@ var levelProgress = document.getElementById('levelProgress');
 var accuracybar = document.getElementById('accuracybar');
 var progressAccuracyBar = document.getElementById('progressAccuracyBar');
 
-
-var timer = 20;
+var TIMER = 60;
+var timer = TIMER;
 var SPEED = 0.3;
+var POSSIBLE_MISSED = 3;
 var value = 0;
+var sound;
 
 var ctx = canvas.getContext("2d"),
   W = canvas.width, // simplified for demo
@@ -26,7 +28,7 @@ var ctx = canvas.getContext("2d"),
 
 //------------- Globalvariables --------------------- 
 
-var ball;
+var text;
 var renderintervalId;
 var levelIntervalId;
 var levelIntervalTimerId;
@@ -35,20 +37,18 @@ var scoringWord="";
 
 var score = {
   correct:0,
-  missed:0,
+  missed:POSSIBLE_MISSED,
   totalWords:0,
   level:0,
-  overAllAccuracy: 0,
-
-  
+  overallAccuracy:0,
   increaseTotalWords: function(){
     this.totalWords += 1;
   },
   increaseCorrect: function(){
     this.correct += 1;
   },
-  increaseMissed: function(){
-    this.missed += 1;
+  decreaseMissed: function(){
+    this.missed -= 1;
   },
   getCorrect: function(){
     return this.correct;
@@ -58,9 +58,9 @@ var score = {
   },
   getAccuracy:function(){
     if(!this.totalWords){
-      return 0+"%";
+      return 0;
     }
-    return Math.floor((this.correct/this.totalWords)*100) + "%";
+    return Math.floor((this.correct/this.totalWords)*100);
   },
   getLevelBar: function(){
     return 20*this.level;
@@ -72,26 +72,29 @@ var score = {
       return this.level + "/5";
   },
   // ---------------------------start---------------------
+  updateOverallAccuracy: function(){
+    let currentAccuracy = this.getAccuracy();
+    let previousOverallAccuracy = this.overallAccuracy;
+    let level = this.level;
+    this.overallAccuracy = (Math.floor(previousOverallAccuracy*(level-1)+currentAccuracy)/level);
+  },
   getOverallAccuracy:function(){
-    if(!this.totalWords){
-      return 0+"%";
-    }
-    return Math.floor((this.correct/this.totalWords)*100) + "%";
+    return this.overallAccuracy;
   },
 
   getOverallAccuracyBar: function(){
-    return 
+    return Math.floor((this.correct/this.totalWords)*100);
   },
   // ------------------------end--------------------------
   reset: function(){
-    this.correct = 0; this.missed = 0; this.totalWords = 0;
+    this.correct = 0; this.missed = POSSIBLE_MISSED; this.totalWords = 0;
   }
 }
 
 
-//------------------  BALL/TEXT CLASS  ----------------//
+//------------------  TEXT CLASS  ----------------//
 
-function Ball(text) {
+function Text(text) {
   this.x = 220;
   this.y = 3;
   this.color = "#333";
@@ -100,27 +103,31 @@ function Ball(text) {
   this.text = text;
 }
 
-Ball.prototype = {
+Text.prototype = {
   draw: function () {
     ctx.fillStyle = this.color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "white";
-    ctx.font = "50px Arial";
-    ctx.strokeText(this.text, this.x, this.y);
+    ctx.font = "60px Comic Sans MS";
+    ctx.fillText(this.text, this.x, this.y);
   },
 
   update: function () {
     ctx.fillStyle = "white";
-    ctx.font = "50px Arial";
+    ctx.font = "60px Comic Sans MS";
     this.y += this.vy;
     // this.vy += gravity;
-    ctx.strokeText(this.text, this.x, this.y);
-    if (this.y > H) {
+    ctx.fillText(this.text, this.x, this.y);
+    if (this.y > H) {       // word touches the bottom.
       clearInterval(renderintervalId);
       renderintervalId = null;
       clearCanvas();
-      score.increaseMissed();
+      score.decreaseMissed();
       renderScore();
+      if(score.getMissed()==0){    
+        // alert("YouLost");
+        clearLevel(true);
+      }
     }
   },
 };
@@ -130,10 +137,10 @@ function clearCanvas() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function renderBall() {
+function renderText() {
   clearCanvas();
-  ball.draw();
-  ball.update();
+  text.draw();
+  text.update();
 }
 
 
@@ -143,13 +150,22 @@ function clearRenderInterval(){
   clearCanvas();
 }
 
-function clearLevel(){
+function clearLevel(isGameOver = false){
+  // clear timer
+  clearInterval(levelIntervalTimerId);
+  levelIntervalTimerId = null;
+  // clear level
   clearInterval(levelIntervalId);
   levelIntervalId = null;
+  // clear word's movement
   clearRenderInterval();
   input.value = "";
+  if(!isGameOver){
+    score.increaseLevel();
+    score.updateOverallAccuracy();
+  }
   score.reset();
-  score.increaseLevel();
+  timer=TIMER;
   renderScore();
 }
 
@@ -157,14 +173,17 @@ function clearLevel(){
 //------------------------  GAME STARTS FROM HERE ----------------------- //
 
 start.addEventListener("click", function () {
+  renderGame();
+});
 
+function renderGame(){
   // START LEVEL
+  renderScore();
   levelIntervalId = setInterval(startGameLevel,10);  
-
   // START TICK TICK
    levelIntervalTimerId = setInterval(countDown,1000);
+}
 
-});
 
 // --- calulate the time ---
 
@@ -176,11 +195,12 @@ function countDown(){
   } else if(timer == 0){
     //game end for current level
     clearLevel();
-    clearInterval(levelIntervalTimerId);
     
    }
    // time display
-   time.innerHTML = timer;
+   time.innerHTML = `${timer} <span class='text-medium'>sec</span>`;
+  //  list.insertBefore(newItem, list.childNodes[0]);
+  // time.insertBefore(timer, time.childNodes[2] );
 }
 //------------------------   HANDLING THE WORDS MOVEMENT ----------------------- //
 
@@ -192,8 +212,8 @@ function startGameLevel() {
   // logic for word selection
   let word = getMeRandomWord();
   scoringWord = word
-  ball = new Ball(word);
-  renderintervalId = setInterval(renderBall, 10);
+  text = new Text(word);
+  renderintervalId = setInterval(renderText, 10);
 }
 
 
@@ -220,17 +240,13 @@ function handleInput(){
 function renderScore(){
   wrong.innerHTML = score.getMissed();
   correct.innerHTML = score.getCorrect();
-  accuracy.innerHTML = score.getAccuracy();
+  accuracy.innerHTML = `${score.getAccuracy()} <span class='text-medium'>%</span>`;
   level.innerHTML = score.getLevel();
   levelProgress.value = score.getLevelBar();
-  // -----------------------------------start-----------------------------------
-  accuracybar.innerHTML = score.getOverallAccuracy();
-  progressAccuracyBar.innerHTML = score.getOverallAccuracyBar();
+  accuracybar.innerHTML = score.getOverallAccuracy()+"%";
+  progressAccuracyBar.value = score.getOverallAccuracy();
  
-  // ----------------------------------end----------------------------------------
 };
-
-
 
 // ------------------------ random words --------------------------------------------
 
